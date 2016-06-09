e1549dc28a78c29f62f7a9f0d1c5974692a569c9
tangyuhua <yhtang_xt@163.com>
2016-06-09 16:45:56 +0800

    Initial Commit

---
+++ microphoneArray/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  microphoneArray
+//
+//  Created by Tang yuhua on 16/6/9.
+//  Copyright © 2016年 Tang yuhua. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
